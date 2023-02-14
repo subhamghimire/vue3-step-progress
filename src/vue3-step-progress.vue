3af554aacb38a6ea0cadb76be3ea748dbe57250f
subhamghimire <shekhar.ghimire24@gmail.com>
2023-02-14 21:28:03 +0545

    Added vertical progress when mobile view

--- src/vue3-step-progress.vue
+++ src/vue3-step-progress.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="step_progress_main">
     <div class="container">
-      <ul id="progressbar">
+      <ul class="progressbar">
         <li
           v-for="(step, index) in steps"
           :key="'step_' + index"
-          :class="
-            activeStep == index + 1 ? 'active' : '' + isCompleted(index + 1)
-          "
+          :class="activeStep == index + 1 ? 'active' : '' + isCompleted(index + 1)"
+          class="progress__item"
         >
           {{ step }}
         </li>
@@ -17,7 +16,7 @@
 </template>
 
 <script>
-import { defineComponent, toRefs, watch, ref } from "vue";
+import { defineComponent, toRefs, watch, ref } from 'vue';
 
 export default defineComponent({
   props: {
@@ -44,8 +43,8 @@ export default defineComponent({
         (activeStep.value > oldStep.value && index < activeStep.value) ||
         (index < activeStep.value && oldStep.value > index)
       )
-        return "completed";
-      return "";
+        return 'completed';
+      return '';
     }
     return {
       isCompleted,
@@ -63,78 +62,126 @@ ul {
   margin-inline-end: 0px;
   padding-inline-start: 40px;
 }
-.step_progress_main {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+
+@media only screen and (min-width: 768px) {
+  .container {
+    width: 100%;
+  }
+  .step_progress_main {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  .progressbar {
+    counter-reset: step;
+    width: 100% !important;
+  }
+  .progress__item {
+    position: relative;
+    list-style: none;
+    float: left;
+    width: 33%;
+    text-align: center;
+    min-height: 10px;
+    color: #707070;
+  }
+  .progress__item:before {
+    content: counter(step);
+    counter-increment: step;
+    width: 30px;
+    height: 30px;
+    border: 1px solid #c6c6c6;
+    display: block;
+    margin: 0 auto 10px auto;
+    border-radius: 50%;
+    line-height: 30px;
+    background: #c6c6c6;
+    color: #707070;
+    text-align: center;
+    font-weight: bold;
+    margin-bottom: -4rem;
+    position: relative;
+    z-index: 9;
+  }
+  .progress__item:after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background: #e3e3e3;
+    top: 15px;
+    left: -50%;
+    z-index: -1;
+  }
 }
-.container {
-  width: 100%;
-  z-index: 1;
-  margin: 0 auto;
-}
-#progressbar {
-  counter-reset: step;
-  width: 100% !important;
-}
-#progressbar li {
-  position: relative;
-  list-style: none;
-  float: left;
-  width: 33%;
-  text-align: center;
-  min-height: 10px;
-  color: #707070;
-}
-#progressbar li:before {
-  content: counter(step);
-  counter-increment: step;
-  width: 30px;
-  height: 30px;
-  border: 1px solid #c6c6c6;
-  display: block;
-  margin: 0 auto 10px auto;
-  border-radius: 50%;
-  line-height: 25px;
-  background: #c6c6c6;
-  color: #707070;
-  text-align: center;
-  font-weight: bold;
-  margin-bottom: -4rem;
-  position: relative;
-  z-index: 9;
-}
-#progressbar li:after {
-  content: "";
-  position: absolute;
-  width: 100%;
-  height: 2px;
-  background: #e3e3e3;
-  top: 15px;
-  left: -50%;
-  z-index: -1;
-}
-#progressbar li.active {
+
+.progress__item.active {
   color: #045cab;
 }
 
-#progressbar li.active:before,
-#progressbar li.completed:before {
+.progress__item.active:before,
+.progress__item.completed:before {
   background: #045cab;
   color: white;
   border: 1px solid #045cab;
 }
 
-#progressbar li.completed:before {
-  content: "✔";
+.progress__item.completed:before {
+  content: '✔';
 }
 
-#progressbar li.active:after,
-#progressbar li.completed:after {
+.progress__item.active:after,
+.progress__item.completed:after {
   background: #045cab;
 }
 
-#progressbar li:first-child:after {
+.progress__item:first-child:after {
   content: none;
+}
+@media only screen and (max-width: 768px) {
+  .step_progress_main {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .progressbar {
+    counter-reset: step;
+    position: relative;
+  }
+  .progress__item {
+    position: relative;
+    list-style: none;
+    color: #707070;
+    min-height: 100px;
+  }
+  .progress__item:before {
+    content: counter(step);
+    counter-increment: step;
+    width: 30px;
+    height: 30px;
+    border: 1px solid #c6c6c6;
+    display: block;
+    margin: 20px auto 10px auto;
+    border-radius: 50%;
+    line-height: 30px;
+    background: #c6c6c6;
+    color: #707070;
+    text-align: center;
+    font-weight: bold;
+    margin-bottom: -4rem;
+    position: absolute;
+    z-index: 9;
+  }
+  .progress__item:after {
+    content: '';
+    height: 35px;
+    width: 1px;
+    background: #e3e3e3;
+    position: absolute;
+    top: -35px;
+    left: 1rem;
+    border: none;
+    z-index: -1;
+  }
 }
 </style>
